c3a293e686c19e92ad41b1eabd9778386c633997
Bjoern Meyer <bjoern@textcontrol.com>
2022-04-22 14:42:00 +0200

    Add project files.

---
+++ tx-context-toolbar/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace tx_context_toolbar.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
