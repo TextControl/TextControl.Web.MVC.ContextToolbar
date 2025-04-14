13ff51831814de4ec207478c8f75180305e6f116
Zuzanna Kwiatkowska <zuzanna.kwiatkowska@textcontrol.com>
2025-04-14 13:26:22 +0200

    Updated to TX33

--- tx-context-toolbar/Program.cs
+++ tx-context-toolbar/Program.cs
@@ -1,3 +1,5 @@
+using TXTextControl.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,19 +18,19 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 // serve static linked files (JavaScript and CSS for the editor)
-app.UseStaticFiles(new StaticFileOptions {
-	FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
-	   System.IO.Path.Combine(System.IO.Path.GetDirectoryName(
-		   System.Reflection.Assembly.GetEntryAssembly().Location),
-		   "TXTextControl.Web")),
-	RequestPath = "/TXTextControl.Web"
-});
+//app.UseStaticFiles(new StaticFileOptions {
+//	FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
+//	   System.IO.Path.Combine(System.IO.Path.GetDirectoryName(
+//		   System.Reflection.Assembly.GetEntryAssembly().Location),
+//		   "TXTextControl.Web")),
+//	RequestPath = "/TXTextControl.Web"
+//});
 
 // enable Web Sockets
 app.UseWebSockets();
 
 // attach the Text Control WebSocketHandler middleware
-app.UseMiddleware<TXTextControl.Web.WebSocketMiddleware>();
+app.UseTXWebSocketMiddleware();
 
 app.UseRouting();
 
